25d7192ea0c2aafc09ce7776c2967957391b5a60
“kodama0621” <eryuhikari@gmail.com>
2021-04-01 15:53:09 +0000

    [Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  attachment :profile_image
+
+  validates :name, presence: true
+  validates :name, length: {minimum: 2}
+  validates :name, length: {maximum: 20}
+  validates :introduction, length: {maximum: 50}
+  validates :name, uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
-  resources :books
   devise_for :users
- root to: 'homes#top'
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :books, only: [:new, :create, :index, :show, :destroy ,:edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
