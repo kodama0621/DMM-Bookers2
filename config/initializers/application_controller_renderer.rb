086cbfc7de990b516693a81bae29073b94371f62
“kodama0621” <eryuhikari@gmail.com>
2021-04-02 12:06:45 +0000

    [Add]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '42b26731cb8cd05f47cd26bc89f88cb4e3fcaa044c6b2982e234d53dc798d184103e080ecc3c2516ff818d083dabc1c48b31bc7c981dc2934d71dac70e28a530'
